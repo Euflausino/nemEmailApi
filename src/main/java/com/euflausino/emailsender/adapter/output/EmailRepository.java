92ce234e7cf01c33131290a74f855b3f3452a63e
Bruno Euflausino <brunoeuflausino@gmail.com>
2026-01-25 19:17:02 -0300

    initial commit

--- src/main/java/com/euflausino/emailsender/adapter/output/EmailRepository.java
+++ src/main/java/com/euflausino/emailsender/adapter/output/EmailRepository.java
@@ -2,6 +2,5 @@ package com.euflausino.emailsender.adapter.output;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface EmailRepository extends JpaRepository<EmailJPA, String> {
 }
